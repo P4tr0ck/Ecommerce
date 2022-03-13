ed2a0eaa17854a4a95d9c750d320758e2fb3a4ac
Patrick <patrickhferreirab@outlook.com>
2022-03-13 20:01:59 -0300

    Primeira parte do Ecommerce(Design)

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "../images/slider1.png",
+      title: "SUMMER SALE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "f5fafd",
+    },
+    {
+      id: 2,
+      img: "../images/slider2.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 3,
+      img: "../images/slider3.png",
+      title: "LOUNGEWEAR LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "../images/categorie1.webp",
+      title: "SHIRT STYLE!",
+    },
+    {
+      id: 2,
+      img: "../images/categorie2.jpeg",
+      title: "LOUNGEWEAR LOVE",
+    },
+    {
+      id: 3,
+      img: "../images/categorie3.jpeg",
+      title: "LIGHT JACKETS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"../images/id1.png",
+    },
+    {
+      id:2,
+      img:"../images/id2.png",
+    },
+    {
+      id:3,
+      img:"../images/id3.png",
+    },
+    {
+      id:4,
+      img:"../images/id4.png",
+    },
+    {
+      id:5,
+      img:"../images/id5.png",
+    },
+    {
+      id:6,
+      img:"../images/id6.png",
+    },
+    {
+      id:7,
+      img:"../images/id7.png",
+    },
+    {
+      id:8,
+      img:"../images/id8.png",
+    },
+  ]
